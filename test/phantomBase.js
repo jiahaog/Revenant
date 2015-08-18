ccbfcbf7f3e304b3ba6c84152ce91daa48613ea6
Jia Hao <jiahaog@gmail.com>
2015-08-19 02:16:59 +0800

    Change test case to test with multiple urls

--- test/phantomBase.js
+++ test/phantomBase.js
@@ -2,18 +2,27 @@
  * Created by JiaHao on 18/8/15.
  */
 
+
+var async = require('async');
 var phantomBase = require('./../lib/phantomBase');
+
 
 const testUrls = ['http://techcrunch.com/', 'https://www.facebook.com/'];
 
 
-describe('Base PhantomJS functions', function () {
-    this.timeout(30000);
+describe('Testing base PhantomJS functions', function () {
+    this.timeout(50000);
     it('Can open a page', function (done) {
-        phantomBase.openPage(testUrls[1], function (error, page, ph) {
-            ph.exit();
-            done(error);
-        })
+
+        async.each(testUrls, function (testUrl, callback) {
+            phantomBase.openPage(testUrl, function (error, page, ph) {
+                ph.exit();
+                callback(error);
+            });
+        }, function (error) {
+           done(error);
+        });
+
     })
 
 
